8af6d1951b6a3fd8b2ac27f9474cc2dff8a17241
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-04-30 17:45:40 +0300

    adde a the initilizing function for the news objects.

---
+++ app/models.py
@@ -0,0 +1,11 @@
+class News:
+    '''
+    News class to define News Objects.
+    '''
+    def __init__(self, source, author, title, content, description, url):
+        self.source = source
+        self.author = author
+        self.title = title
+        self.content = content
+        self. description = description
+        self.url = url
