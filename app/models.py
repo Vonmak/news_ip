00a091acc0862f95407ecbbeb0f51f32530d30e4
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-03 19:50:00 +0300

    Added a class source in the models.py.

--- app/models.py
+++ app/models.py
@@ -1,11 +1,26 @@
+class Source:
+    '''
+    Class that defines sources objects
+    '''
+    def __init__(self,id,name,url):
+        self.id=id
+        self.name=name
+        self.url=url
+      
+
 class News:
     '''
-    News class to define News Objects.
+    News class to define News Objects
     '''
-    def __init__(self, source, author, title, content, description, url):
-        self.source = source
-        self.author = author
+    all_news = []
+
+    def __init__(self,title, author, description, content, url, urlToImage, publishedAt):
+        
         self.title = title
+        self.author = author
+        self.description =description
         self.content = content
-        self. description = description
         self.url = url
+        self.urlToImage =urlToImage
+        self.publishedAt = publishedAt
+        
