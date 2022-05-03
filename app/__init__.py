44c77f060c48875c8c0e69d79121a097e514bd5d
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-03 20:44:24 +0300

    registering the blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,10 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     
+    # Registering the blueprints
+    from .main import main  as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
     
     
     return app
