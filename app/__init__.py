93c996d81aa9568ea51635ba5237fd50259eba3e
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-03 20:41:04 +0300

    added a blueprint and initialized it in as a main

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 
+from flask import Blueprint
+main = Blueprint('main',__name__)
+from . import errors
+
 bootstrap = Bootstrap()
 
 def create_app(config_name):
