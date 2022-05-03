6ee0c91ecd329bee4c884c7c9e745b8055a1918b
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-03 20:52:40 +0300

    added other functions and routes that point to their respectful templates.

--- app/main/views.py
+++ app/main/views.py
@@ -17,3 +17,23 @@ def index():
         return redirect(url_for('search',news_name=search_news))
     else:
         return render_template('index.html', title= title, sourceList= sourceSamples)
+
+
+@main.app.route('/business')
+def business():
+    business_news = get_articles('business')
+    
+    return render_template('business.html', business = business_news)
+
+@main.app.route('/technology')
+def technology():
+    technology_news = get_articles('technology')
+    
+    return render_template('technology.html', technology = technology_news)
+     
+@main.app.route('/sports')
+def sports():
+    sports_news = get_articles('sports')
+    
+    return render_template('sports.html', sports = sports_news)
+
