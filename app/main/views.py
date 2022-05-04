a6377c1850280210dafc82953c3ac8080177d681
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-04 06:15:54 +0300

    updated the @main routes.

--- app/main/views.py
+++ app/main/views.py
@@ -4,34 +4,34 @@ from ..requests import get_articles, get_sources
 
 
 #views
-@main.app.route('/')
+@main.route('/')
 def index():
     '''
     view root page function that returns the index page data
     '''
-    sourceSamples = get_sources()
+    sourceList = get_sources()
     
     title= 'The News center'
     search_news = request.args.get('news_query')
     if search_news:
         return redirect(url_for('search',news_name=search_news))
     else:
-        return render_template('index.html', title= title, sourceList= sourceSamples)
+        return render_template('index.html', title= title, sourceList= sourceList)
 
 
-@main.app.route('/business')
+@main.route('/business')
 def business():
     business_news = get_articles('business')
     
     return render_template('business.html', business = business_news)
 
-@main.app.route('/technology')
+@main.route('/technology')
 def technology():
     technology_news = get_articles('technology')
     
     return render_template('technology.html', technology = technology_news)
      
-@main.app.route('/sports')
+@main.route('/sports')
 def sports():
     sports_news = get_articles('sports')
     
